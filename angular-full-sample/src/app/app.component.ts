d310c255ba59eb6905082921a1ef62ff143fb243
Joseph <nguld@itsj-group.com>
2021-01-08 15:44:20 +0700

    updated app component

--- angular-full-sample/src/app/app.component.ts
+++ angular-full-sample/src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {Menu} from './core/nav/menu';
+import {AuthService} from './core/services/auth.service';
+import {TranslateService} from 'ng2-translate';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-full-sample';
+export class AppComponent implements OnInit {
+
+  title = 'Tour of Heroes and Villains';
+  menus: Menu[] = [
+    new Menu('Heroes', '/heroes', 'fa-flash'),
+    new Menu('Villains', '/villains', 'fa-user-secret ')
+  ];
+  languages = [{id: 'en', value: 'English'}, {id: 'es', value: 'Spanish'}];
+
+  constructor(
+    // @Inject('AuthService')
+    // private authService: AuthService,
+    // private translate: TranslateService,
+    // private router: Router
+  ) {
+    // this language will be used as a fallback when a translation isn't found in the current language
+    // translate.setDefaultLang('en');
+  }
+
+  ngOnInit(): void {
+    console.log('test');
+  }
+
+  isActive(): boolean {
+    // return this.authService.isUserLogged();
+    return null;
+  }
+
+  getUser(): string {
+    // return this.authService.getUserLogged();
+    return null;
+  }
+
+  onLogout(): void {
+    // this.authService.logout();
+    // this.router.navigate(['/login']);
+  }
 }
--- angular-full-sample/src/app/app.module.ts
+++ angular-full-sample/src/app/app.module.ts
@@ -1,8 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {TranslateLoader, TranslateModule, TranslateService} from 'ng2-translate';
+import {Http} from '@angular/http';
+import {TranslateLoaderFactory} from './core/utils/app.translate.factory';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TranslateModule.forRoot({
+      provide: TranslateLoader,
+      useFactory: TranslateLoaderFactory,
+      deps: [Http]
+    })
   ],
-  providers: [],
+  providers: [TranslateService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
