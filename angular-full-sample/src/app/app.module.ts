c63cc2a36ba34b51ceb49da7cff1301f3f08b360
Joseph <nguld@itsj-group.com>
2021-01-08 16:04:45 +0700

    added nav component

--- angular-full-sample/src/app/app.module.ts
+++ angular-full-sample/src/app/app.module.ts
@@ -6,10 +6,12 @@ import {AppComponent} from './app.component';
 import {TranslateLoader, TranslateModule, TranslateService} from 'ng2-translate';
 import {Http} from '@angular/http';
 import {TranslateLoaderFactory} from './core/utils/app.translate.factory';
+import { NavComponent } from './core/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
