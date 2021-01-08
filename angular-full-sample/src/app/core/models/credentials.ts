4cd13d38ec7b05ac9d8c30d3dac4a40f249f2dee
Joseph <nguld@itsj-group.com>
2021-01-08 10:56:55 +0700

    added models

---
+++ angular-full-sample/src/app/core/models/credentials.ts
@@ -0,0 +1,11 @@
+export class Credentials {
+
+  clientId: string;
+  clientSecret: string;
+
+  constructor(clientId: string, clientSecret: string) {
+    this.clientId = clientId;
+    this.clientSecret = clientSecret;
+  }
+
+}
