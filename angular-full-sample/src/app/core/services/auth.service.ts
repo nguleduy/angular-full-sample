e9bcd0a849e21f3ac851d55d6e518becc40cbbb9
Joseph <nguld@itsj-group.com>
2021-01-08 10:22:43 +0700

    added interface Auth service

---
+++ angular-full-sample/src/app/core/services/auth.service.ts
@@ -0,0 +1,29 @@
+export interface AuthService {
+
+  /**
+   * Determine if there is a user correctly logged in the app
+   */
+  isUserLogged(): boolean;
+
+  /**
+   * Returns the name of the user logged in the app
+   */
+  getUserLogged(): string;
+
+  /**
+   * Returns the token stored after login
+   */
+  getToken(): string;
+
+  /**
+   * Log-in into the app using user and password
+   * @param username a valid user id
+   * @param password a valid password
+   */
+  login(username: string, password: string): void;
+
+  /**
+   * Logout from the app
+   */
+  logout(): void;
+}
