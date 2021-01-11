6f2fdd36f8c4889ab091dd55718a9104b48274b5
Joseph <nguld@itsj-group.com>
2021-01-11 14:28:17 +0700

    updated environment

--- angular-full-sample/src/environments/environment.ts
+++ angular-full-sample/src/environments/environment.ts
@@ -3,7 +3,21 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  envName: 'dev',
+  apiConfig: {
+    apiEnv: 'dev',
+    timeExpired: 20,
+    credentials: {
+      clientId: 'trustedclient',
+      clientSecret: 'trustedclient123'
+    },
+    apiUrls: [
+      {id: 'HEROES_SERVICE_URL', url: 'app/heroes'},
+      {id: 'VILLAINS_SERVICE_URL', url: 'app/villains'},
+      {id: 'AUTH_SERVICE_URL', url: 'http://127.0.0.1:9001/hague-auth/oauth/token'}
+    ],
+  }
 };
 
 /*
